4c00efed88e2c720b63def8acf55694ae7b88273
Piotr Fijoł <piotr.fijol1@gmail.com>
2022-08-26 20:11:55 +0200

    Make a round handling function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,13 @@
 function getComputerChoice() {
     return Math.floor(Math.random() * 3);
 }
+
+function playRound(playerChoice, computerChoice) {
+    if(playerChoice === computerChoice) {
+        return "It's a draw!";
+    } else if ((playerChoice + 1) % 3 === computerChoice) {
+        return "Computer wins!";
+    } else {
+        return "You won!";
+    }
+}
