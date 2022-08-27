d5d2ee5a1bfb1eeb9d3931c0b8702433ff8af2d8
Piotr Fijoł <piotr.fijol1@gmail.com>
2022-08-27 17:01:05 +0200

    Implement a main function for RPS game
    The function is a wrapper for actual rps function with an infinite loop in it.
    I decided to make it a generator function because Im using an infinite while loop
    inside of it so it either would crash the web browser or If i'd change the
    condition it would execute instantly looping all 5 times in a split of second
    so the user makes a choice only once and the answers is the same for all the rounds.

--- js/main.js
+++ js/main.js
@@ -4,10 +4,64 @@ function getComputerChoice() {
 
 function playRound(playerChoice, computerChoice) {
     if(playerChoice === computerChoice) {
-        return "It's a draw!";
+        printText("It's a draw!");
+        return 0;
     } else if ((playerChoice + 1) % 3 === computerChoice) {
-        return "Computer wins!";
+        printText("Computer wins!");
+        return 1;
     } else {
-        return "You won!";
+        printText("You won!");
+        return 2;
+        
     }
+}
+
+function playGame() {
+    const options = ['rock', 'paper', 'scissors'];
+
+    printText("\n\nWhich one do you choose?")
+    
+    terminal.eventAwait = _playGame();
+    
+    function* _playGame() {
+        const MAX_ROUNDS = 5;
+        let playedRounds = 0;
+        let playerScore = 0, computerScore = 0;
+
+        while(true) {
+            let choice = options.indexOf(terminal.input);
+            if(choice !== -1) {
+                let round = playRound(choice, getComputerChoice()) 
+                if(round === 0){
+                    playerScore++;
+                    computerScore++;
+                } else if(round === 1){
+                    computerScore++;
+                } else {
+                    playerScore++;
+                }
+
+                playedRounds++;
+
+                if(playedRounds === MAX_ROUNDS) {
+                    printText(`Final score: ${playerScore} - ${computerScore}`)
+                    if(playerScore === computerScore)
+                        printText("That's a tie, it was a close game.");
+                    else if(playerScore > computerScore)
+                        printText("Hats off, you beat a computer in RPS game.");
+                    else
+                        printText("You got outplayed by a machine, come on. You can do better than this!");
+
+                    printText("End of the game!\nType 'rps' to play again.\n\n")
+                    terminal.eventAwait = null;
+                    terminal.status = FREE;
+                }
+            } else {
+                printText("Wrong Option!");
+            }
+            yield;
+        }
+
+    }
+
 }
